808ceb87bff643f0765bf563958a0fd8debe3b1d
vinaypuppal <me@vinaypuppal.com>
2021-09-21 00:58:41 +0530

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,15 @@ import multipage from "vite-plugin-multipage";
 export default defineConfig({
   plugins: [
     multipage({
-      // This is an optional object, defaults as follows:
-      mimeCheck: true /* mimeCheck: Set the MIME type on each request using
-                         `mime-types.lookup()` */,
-      open: "/challenges/order-summary-component/index.html" /* open: Path to load when starting the server.
-                   May be left empty and not open anything. */,
-      pageDir:
-        "challenges" /* pageDir: Path to the directory with the pages. */,
-      purgeDir: "challenges" /* purgeDir: Path to be removed after building.
-                           May be left empty to remove nothing. */,
-      removePageDirs: false /* removePageDirs: Change the final result from
-                               "./page/index.html" to "./page.html". */,
-      rootPage: "index.html" /* rootPage: The entry point into each page. */,
+      mimeCheck: true,
+      open: "/challenges/order-summary-component/index.html",
+      pageDir: "challenges",
+      purgeDir: "challenges",
+      removePageDirs: false,
+      rootPage: "index.html",
     }),
   ],
-
   build: {
-    target: "esnext",
     polyfillDynamicImport: false,
   },
 });
